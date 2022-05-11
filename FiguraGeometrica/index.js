c04e4f4816175c0f431ab2b1fc15b3ed602b9f39
xoyoc <xoyoc_l2@hotmail.com>
2022-05-10 23:16:20 -0500

    Estamos en el paso dos cuadrado y triángulo

--- FiguraGeometrica/index.js
+++ FiguraGeometrica/index.js
@@ -1,1 +1,47 @@
-console.log("Prueba de archivo")
+// Código del cuadrado
+
+const lCuadrado = 5;
+console.group("Cuadrado");
+    console.log("El lado del cuadrado mide: " + lCuadrado + "cm");
+
+    const perimetroCuadrado = lCuadrado * 4;
+
+    console.info("EL pedimetro del cuadrado es: " + perimetroCuadrado +"cm" );
+
+    const areaCudrado = lCuadrado * lCuadrado;
+
+    console.info("El área del cuadrado es: " + areaCudrado + "cm^2");
+console.groupEnd();
+// Código del cuadrado
+
+// Código del traingulo
+
+const lTriangulo1 = 6;
+const lTriangulo2 = 6;
+const baseTriangulo = 4;
+const alturaTriangulo = Math.sqrt((lTriangulo1 ** 2)-(baseTriangulo **2)).toFixed(1);
+
+console.group("Triángulo");
+console.log(
+    "Los lados del triángulo son: primer lado :"
+    + lTriangulo1 
+    + "cm segundo lado :" 
+    + lTriangulo2 
+    + "cm la base mide :" 
+    + baseTriangulo 
+    + "cm"
+);
+
+const perimetroTriangulo = lTriangulo1+lTriangulo2+baseTriangulo;
+console.info(
+    "El perimetro del triángulo es: " 
+    + perimetroTriangulo 
+    + "cm"
+);
+
+const areaTriangulo = (baseTriangulo * alturaTriangulo) / 2;
+console.log(alturaTriangulo)
+console.info("La área del triángulo es: " + areaTriangulo +"cm^2");
+
+console.groupEnd();
+
